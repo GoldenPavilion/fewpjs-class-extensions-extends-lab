03519bd43477817f40534b26c056a16bfccd7f49
GoldenPavilion <rcorystine@gmail.com>
2021-08-31 17:56:15 -0400

    returns true if triangle and square are valid

--- index.js
+++ index.js
@@ -17,9 +17,23 @@ class Polygon {
 }
 
 class Triangle extends Polygon {
-
+    get isValid(){
+        if(this.sides[0] + this.sides[1] > this.sides[2] &&
+           this.sides[0] + this.sides[2] > this.sides[1] &&
+           this.sides[1] + this.sides[2] > this.sides[0]){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 class Square extends Polygon {
-
+    get isValid(){
+        if(this.sides[0] === this.sides[1] === this.sides[2] === this.sides[3]){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
