b81e29a2d91203c2f2b304926a7ffd45ea52db33
GoldenPavilion <rcorystine@gmail.com>
2021-08-31 17:36:17 -0400

    polygon parent class working

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // Your code here
 
 class Polygon {
+    constructor(sides){
+        this.sides = sides;
+    }
 
+    get countSides(){
+        return this.sides.length;
+    }
+
+    get perimeter(){
+        return this.sides.reduce(function(acc, current){
+            return acc + current;
+        }, 0)
+    }
 }
 
 class Triangle extends Polygon {
@@ -9,5 +21,5 @@ class Triangle extends Polygon {
 }
 
 class Square extends Polygon {
-    
+
 }
